1e421c8059e87c4f53be878b6c3cc2c29f4fe6cb
Alejandro Salgado G <asalgad2@eafit.edu.co>
2019-10-29 08:39:05 -0600

    Adding first part of code to save additions

--- TomBill/functions.py
+++ TomBill/functions.py
@@ -7,9 +7,11 @@ def register_bill(data):
     bill = Bill(date=datetime.now())
     bill.save()
 
-    total  = register_products(data, sodas, bill)
+    total  = register_products(data, double_sodas, bill)
+    total += register_products(data, sodas, bill)
     total += register_products(data, bakery, bill)
     total += register_products(data, coffe, bill)
+    total += register_additions(data, additions, bill)
 
     bill.total = total
     bill.save()
@@ -23,3 +25,11 @@ def register_products(data, elements, bill):
             product.save()
             subtotal += price * quantity 
     return subtotal
+
+def register_additions(data, elements, bill):
+    subtotal = 0
+    for id_name, name, price in elements:
+        quantity = int( data[id_name] )
+        print(id_name, quantity)
+    return subtotal
+
