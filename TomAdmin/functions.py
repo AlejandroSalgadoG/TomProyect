9509a4495d017c5e14daa2c268642004b42e32a8
Alejandro Salgado G <asalgad2@eafit.edu.co>
2019-12-04 09:46:25 -0600

    Adding time interval to consult bills

--- TomAdmin/functions.py
+++ TomAdmin/functions.py
@@ -1,4 +1,13 @@
+from datetime import datetime, date
+
 from TomBill.models import Bill, Product
 
-def get_bills_info():
-    return [[bill, bill.product_set.all()] for bill in Bill.objects.all()]
+def get_bills_info(date_from, date_to):
+    if date_from is None:
+        return []
+
+    if date_to is None:
+        date_to = datetime.now()
+
+    bills = Bill.objects.filter(date__gte=date_from, date__lte=date_to)
+    return [[bill, bill.product_set.all()] for bill in bills]
--- TomAdmin/views.py
+++ TomAdmin/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -7,7 +9,16 @@ class Index(TemplateView):
     index_template = 'AdminIndex.html'
 
     def get(self, request):
-        bills = get_bills_info()
+        date_from, date_to = None, None
+
+        if "date_from" in request.GET and request.GET["date_from"] != "":
+            date_from = datetime.strptime(request.GET["date_from"], "%Y-%m-%d")
+
+
+        if "date_to" in request.GET and request.GET["date_to"] != "":
+            date_to = datetime.strptime(request.GET["date_to"], "%Y-%m-%d")
+
+        bills = get_bills_info(date_from, date_to)
         data = {"bills": bills}
 
         return render(request, self.index_template, data)
